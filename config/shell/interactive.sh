84df1389a1167119025e8a0aad1e4cfe4756b1ed
Indrajit <indrajitghosh912@gmail.com>
2024-12-18 23:56:32 +0530

    new command added related to gglg and gglg2

--- config/shell/interactive.sh
+++ config/shell/interactive.sh
@@ -1,4 +1,3 @@
-alias py='python'
 # Activate Python virtual environment
 alias venv="source ./env/bin/activate"
 
@@ -9,5 +8,7 @@ alias ggc="git commit"
 alias ggp="git push"
 alias ggpm="git push origin master"
 alias ggl="git log"
+alias gglg="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all"
+alias gglg2="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'"
 alias ggb="git branch"
 alias ggi="git init"
